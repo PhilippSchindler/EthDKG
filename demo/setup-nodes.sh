aefbdbd442dae42f506f237fe2577e24f0c35309
Philipp Schindler <pschindler@sba-research.org>
2019-02-15 19:08:06 +0100

    DEMO: added background images to indicate currently active node

--- demo/setup-nodes.sh
+++ demo/setup-nodes.sh
@@ -7,6 +7,9 @@ LL=60
 
 read -p "Press enter to start node 1... "
 
+i3-msg workspace 1:node &> /dev/null && terminator --profile whitebig &> /dev/null &
+feh --bg-fill ~/github.com/ethdkg/demo/nums/1.png
+
 xdotool \
 	sleep 1 \
 	type --args 1 "PS1='1$ '; history -c;" key Return \
@@ -14,10 +17,11 @@ xdotool \
 	type --args 1 "python dkg.py --line-length $LL \\" key Return \
 	type --args 1 "run 1 $1" \
 	&
-i3-msg workspace 1:node &> /dev/null && terminator --profile whitebig &> /dev/null &
 
 read -p "Press enter to start nodes 2 and 3... "
 
+i3-msg workspace 2:node &> /dev/null && terminator --profile whitebig &> /dev/null &
+feh --bg-fill ~/github.com/ethdkg/demo/nums/2.png
 xdotool \
 	sleep 1 \
 	type --args 1 "PS1='2$ '; history -c;" key Return \
@@ -25,9 +29,11 @@ xdotool \
 	type --args 1 "python dkg.py --line-length $LL \\" key Return \
 	type --args 1 "run 2 $1" key Return \
 	&
-i3-msg workspace 2:node &> /dev/null && terminator --profile whitebig &> /dev/null &
 sleep 5
 
+
+i3-msg workspace 3:node &> /dev/null && terminator --profile whitebig &> /dev/null &
+feh --bg-fill ~/github.com/ethdkg/demo/nums/3.png
 xdotool \
 	sleep 1 \
 	type --args 1 "PS1='3$ '; history -c;" key Return \
@@ -35,11 +41,12 @@ xdotool \
 	type --args 1 "python dkg.py --line-length $LL \\" key Return \
 	type --args 1 "run 3 $1" key Return \
 	&
-i3-msg workspace 3:node &> /dev/null && terminator --profile whitebig &> /dev/null &
 
 read -p "Press enter to start (adversarial) node 4... "
 
 
+i3-msg workspace 4:node &> /dev/null && terminator --profile whitebig &> /dev/null &
+feh --bg-fill ~/github.com/ethdkg/demo/nums/4.png
 xdotool \
 	sleep 1 \
 	type --args 1 "PS1='4$ '; history -c;" key Return \
@@ -48,11 +55,12 @@ xdotool \
 	type --args 1 "run 4 $1 \\" key Return \
 	type --args 3 "-" "-" "send-invalid-shares 1" \
 	&
-i3-msg workspace 4:node &> /dev/null && terminator --profile whitebig &> /dev/null &
 
 read -p "Press enter to start (faulty) node 5... "
 
 
+i3-msg workspace 5:node &> /dev/null && terminator --profile whitebig &> /dev/null &
+feh --bg-fill ~/github.com/ethdkg/demo/nums/5.png
 xdotool \
 	sleep 1 \
 	type --args 1 "PS1='5$ '; history -c;" key Return \
@@ -61,5 +69,4 @@ xdotool \
 	type --args 1 "run 5 $1 \\" key Return \
 	type --args 3 "-" "-" "abort-after-registration" \
 	&
-i3-msg workspace 5:node &> /dev/null && terminator --profile whitebig &> /dev/null &
 
