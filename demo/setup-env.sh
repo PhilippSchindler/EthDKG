0f641c2147e676f30d066ae3377abf4d84cf3de6
Philipp Schindler <pschindler@sba-research.org>
2019-02-15 13:17:07 +0100

    added script for FC19 demo

--- demo/setup-env.sh
+++ demo/setup-env.sh
@@ -1,5 +1,6 @@
 docker stop $(docker ps -aq) &> /dev/null
 
+
 i3-msg workspace number 1 &> /dev/null; sleep 0.5; xdotool key CTRL+Q;
 i3-msg workspace number 2 &> /dev/null; sleep 0.5; xdotool key CTRL+Q;
 i3-msg workspace number 3 &> /dev/null; sleep 0.5; xdotool key CTRL+Q;
@@ -7,10 +8,9 @@ i3-msg workspace number 4 &> /dev/null; sleep 0.5; xdotool key CTRL+Q;
 i3-msg workspace number 5 &> /dev/null; sleep 0.5; xdotool key CTRL+Q;
 i3-msg workspace number 6 &> /dev/null; sleep 0.5; xdotool key CTRL+Q;
 i3-msg workspace number 7 &> /dev/null; sleep 0.5; xdotool key CTRL+Q;
+i3-msg workspace number 8 &> /dev/null; sleep 0.5; xdotool key CTRL+Q;
+i3-msg workspace number 9 &> /dev/null; sleep 0.5; xdotool key CTRL+Q;
 
-# i3-msg workspace 6:docker/ganache &> /dev/null; sleep 1; xdotool key CTRL+Q; sleep 0.5;
-# i3-msg workspace 7:management &> /dev/null; sleep 1; xdotool key CTRL+Q; sleep 0.5;
-# i3-msg workspace 8: &> /dev/null; sleep 1; xdotool key CTRL+Q; sleep 1;
 
 
 xdotool \
@@ -21,7 +21,7 @@ xdotool \
 	type --args 1 "# docker build -t ethdkg ." key Return \
 	type --args 1 "docker run -p 127.0.0.1:8545:8545 -it ethdkg /bin/bash" key Return \
 	sleep 2 \
-	type --args 1 "ganache-cli --host 0.0.0.0 --blockTime 15" \
+	type --args 1 "ganache-cli --host 0.0.0.0 --blockTime 1800" \
 	&
 i3-msg workspace 6:docker/ganache &> /dev/null && terminator --profile whitebig &> /dev/null &
 
@@ -34,30 +34,29 @@ xdotool \
 	type --args 1 "cd ~/github.com/ethdkg && pipenv shell && exit" key Return \
 	sleep 1 \
 	type --args 1 "PS1='M$ '; cd client; history -c;" key Return \
-	type --args 1 "history -s ../demo/setup-nodes.sh " key Return \
 	type --args 1 "history -s \"python -c 'import utils; utils.mine_blocks(10)'\"" key Return \
-	type --args 1 "history -s \"python dkg.py deploy\"" key Return \
 	type --args 1 "history -s \"python -c 'import utils; print(utils.blockNumber())'\"" key Return \
+	type --args 1 "history -s ../demo/setup-nodes.sh " key Return \
+	type --args 1 "history -s \"python dkg.py deploy\"" key Return \
 	key CTRL+L \
 	&
 i3-msg workspace 7:management &> /dev/null && terminator --profile whitebig &> /dev/null &
 
-
-sleep 5
-
-i3-msg workspace number 6
+sleep 8
 
 
-#xdotool \
-#	sleep 1 \
-#	type --args 1 "cd ~/github.com/ethdkg && pipenv shell && exit" key Return \
-#	sleep 1 \
-#	type --args 1 "PS1='1$ '; cd client; history -c;" key Return \
-#	sleep 1 \
-#	type --args 1 "history -s python dkg.py --line-length 60 run 1 " key Return \
-#	sleep 1 \
-#	key CTRL+L \
-#	&
-#i3-msg workspace 1 &> /dev/null && terminator --profile whitebig &> /dev/null &
-#
-# sleep 5
+i3-msg workspace 8:block-explorer &> /dev/null
+sleep 0.5
+google-chrome --new-window \
+	"https://github.com/PhilippSchindler/ethdkg/tree/master/evaluation/testnet-execution" \
+	"https://ropsten.etherscan.io/address/0x64eB9cbc8AAc7723A7A94b178b7Ac4c18D7E6269" \
+ 	&> /dev/null &
+
+sleep 2
+
+i3-msg workspace 9:presentation &> /dev/null
+sleep 0.5
+google-chrome --new-window \
+	"https://docs.google.com/presentation/d/1Fv_UkQfceXPtr6IPuelkf-8qG-gj4LkBpfrmdKBbw7s/edit#slide=id.g4f94ef9aad_0_668" \ 
+ 	&> /dev/null &
+
--- demo/setup-nodes.sh
+++ demo/setup-nodes.sh
@@ -5,50 +5,61 @@ fi
 
 LL=60
 
+read -p "Press enter to start node 1... "
+
 xdotool \
 	sleep 1 \
 	type --args 1 "PS1='1$ '; history -c;" key Return \
 	key CTRL+L \
-	type --args 1 "python dkg.py --line-length $LL run 1 $1" \
+	type --args 1 "python dkg.py --line-length $LL \\" key Return \
+	type --args 1 "run 1 $1" \
 	&
 i3-msg workspace 1:node &> /dev/null && terminator --profile whitebig &> /dev/null &
-sleep 2
+
+read -p "Press enter to start nodes 2 and 3... "
 
 xdotool \
 	sleep 1 \
-	type --args 1 "PS1='1$ '; history -c;" key Return \
+	type --args 1 "PS1='2$ '; history -c;" key Return \
 	key CTRL+L \
-	type --args 1 "python dkg.py --line-length $LL run 2 $1" \
+	type --args 1 "python dkg.py --line-length $LL \\" key Return \
+	type --args 1 "run 2 $1" key Return \
 	&
 i3-msg workspace 2:node &> /dev/null && terminator --profile whitebig &> /dev/null &
-sleep 2
+sleep 5
 
 xdotool \
 	sleep 1 \
-	type --args 1 "PS1='1$ '; history -c;" key Return \
+	type --args 1 "PS1='3$ '; history -c;" key Return \
 	key CTRL+L \
-	type --args 1 "python dkg.py --line-length $LL run 3 $1" \
+	type --args 1 "python dkg.py --line-length $LL \\" key Return \
+	type --args 1 "run 3 $1" key Return \
 	&
 i3-msg workspace 3:node &> /dev/null && terminator --profile whitebig &> /dev/null &
-sleep 2
+
+read -p "Press enter to start (adversarial) node 4... "
+
 
 xdotool \
 	sleep 1 \
-	type --args 1 "PS1='1$ '; history -c;" key Return \
+	type --args 1 "PS1='4$ '; history -c;" key Return \
 	key CTRL+L \
-	type --args 1 "python dkg.py --line-length $LL run 4 $1" \
+	type --args 1 "python dkg.py --line-length $LL \\" key Return \
+	type --args 1 "run 4 $1 \\" key Return \
+	type --args 3 "-" "-" "send-invalid-shares 1" \
 	&
 i3-msg workspace 4:node &> /dev/null && terminator --profile whitebig &> /dev/null &
-sleep 2
+
+read -p "Press enter to start (faulty) node 5... "
+
 
 xdotool \
 	sleep 1 \
-	type --args 1 "PS1='1$ '; history -c;" key Return \
+	type --args 1 "PS1='5$ '; history -c;" key Return \
 	key CTRL+L \
-	type --args 1 "python dkg.py --line-length $LL run 5 $1" \
+	type --args 1 "python dkg.py --line-length $LL \\" key Return \
+	type --args 1 "run 5 $1 \\" key Return \
+	type --args 3 "-" "-" "abort-after-registration" \
 	&
 i3-msg workspace 5:node &> /dev/null && terminator --profile whitebig &> /dev/null &
-sleep 2
-
-i3-msg workspace 1:node &> /dev/null
 
